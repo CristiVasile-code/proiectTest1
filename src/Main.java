ac3314d7e91eeb1212aa8e462f040d140929a65a
Cristi Vasile <ing_vasile_cristian@yahoo.co.uk>
2022-10-10 16:39:46 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
+    private static final String HELLO_WORLD = "Hello world!";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(HELLO_WORLD);
         System.out.println(5);
+        System.out.prinln(10);
     }
 }
